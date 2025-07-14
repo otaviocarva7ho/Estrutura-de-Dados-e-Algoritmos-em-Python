622af62b7fa946d508def33161959207b5e251e3
Otávio Carvalho <carvalhom7otavioh@gmail.com>
2025-07-14 17:36:56 -0300

    Manipulação de Variáveis e constantes

---
+++ programacao_basica_em_python/manipulacao_de_variaveis_e_constantes.py
@@ -0,0 +1,34 @@
+# Variáveis inteiras
+numero = -3
+numero_jogos = 14
+numero_convidados = 15
+
+print(numero)
+print(numero, numero_convidados)
+
+# Variáveis float(ponto flutuante)
+pi = 3.14
+numero_euler = 2.71
+escala_terremoto = -4.55
+
+print(pi)
+print(numero_euler)
+print(escala_terremoto)
+
+# Strings e chars
+letra = 'a'
+palavra1 = 'linguagem de programação'
+palavra2 = 'Python'
+
+print(letra, palavra1)
+print('Estou aprendendo uma', palavra1)
+print('Está', palavra1, 'se chama', palavra2)
+
+idade = int(input('Digite sua idade: '))
+print('Sua idade é ', idade)
+
+pH = float(input('Qual o pH do solo durante a última medição?'))
+print('o pH medido foi ', pH)
+
+nome = str(input('Qual o seu nome?'))
+print('Seu nome é', nome)
