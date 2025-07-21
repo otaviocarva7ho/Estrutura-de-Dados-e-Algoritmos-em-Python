030cb14779467f2e341e2e4675e39c6c4dc5d87d
Otávio Carvalho <carvalhom7otavioh@gmail.com>
2025-07-20 21:14:47 -0300

    Operações Matemáticas

---
+++ programacao_basica_em_python/operacoes_matematicas.py
@@ -0,0 +1,26 @@
+a = 5
+b = 3
+print(a, b)
+
+print('A soma é', a + b)
+
+print('A subtração é', a - b)
+
+print('A divisão é', a / b)
+
+print('A multiplicação é', a * b)
+
+print('O resto da divisão é', 10 % 3)
+
+print('5 elevado a 4 é', 5**4)
+
+import math
+math.sqrt(81)
+
+# Arredondamento
+casos_doenca = 134
+numero_habitantes = 34432
+casos_por_habitante = casos_doenca / numero_habitantes
+print(casos_por_habitante)
+round(casos_por_habitante, 6)
+print('O número de casos por habitante é de', round(casos_por_habitante, 4))
