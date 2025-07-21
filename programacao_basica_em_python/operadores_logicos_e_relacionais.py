57004aa46c1a88b1d0f11d98cc339e03904cefef
Otávio Carvalho <carvalhom7otavioh@gmail.com>
2025-07-21 13:04:25 -0300

    Operadores lógicos e relacionais

---
+++ programacao_basica_em_python/operadores_logicos_e_relacionais.py
@@ -0,0 +1,20 @@
+# Operadores lógicos
+a = True
+b = False
+print(a, b)
+c = a and b
+print("'A' e 'B' são iguais é", c)
+
+d = a or b
+print("'A' ou 'b' é igual a", d)
+
+print(not a)
+print(not b)
+
+# Operadores relacionais
+print(5 > 3)
+print(5 < 3)
+print(5 >= 3)
+print(5 <= 3)
+print(5 == 3)
+print(5 != 3)
