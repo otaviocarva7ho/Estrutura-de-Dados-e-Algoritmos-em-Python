7542d2c0a2508fd3bb57a938bc3db6a90c9c7c57
Otávio Carvalho <carvalhom7otavioh@gmail.com>
2025-07-22 13:21:25 -0300

    Estrutura de repetição - While

---
+++ programacao_basica_em_python/estrutura_de_repeticao_while.py
@@ -0,0 +1,23 @@
+numero = 1
+while numero < 6:
+    print(numero)
+    numero += 1
+print('---')
+
+numero = 5
+while numero > 0:
+    print(numero)
+    numero -= 1
+
+1 + 2 + 3 + 4 + 5
+
+soma = 0
+numero = 1
+while numero < 6:
+    soma += numero
+    numero += 1
+print(soma)
+
+numero = 12
+while numero < 1 or numero > 10:
+    numero = int(input('Digite um número de 1 a 10: '))
